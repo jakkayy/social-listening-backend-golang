5cc91ba1c6a1152ca832c4394608122daf25cbd9
jakkayy <niti2042547@gmail.com>
2026-02-09 14:28:36 +0700

    add c.JSON in overview.go

--- internal/api/handler/overview.go
+++ internal/api/handler/overview.go
@@ -1,10 +1,8 @@
 package handler
 
 import (
+	"net/http"
 	"social-listening-backend-golang/internal/config"
-	"social-listening-backend-golang/internal/domain"
-	"social-listening-backend-golang/internal/ingestion/collector"
-	"social-listening-backend-golang/internal/processing"
 	"social-listening-backend-golang/internal/storage"
 
 	"github.com/gin-gonic/gin"
@@ -14,20 +12,16 @@ func OverviewHandler(c *gin.Context) {
 	db := config.NewDB()
 	defer db.Close()
 
-	commentRepo := storage.NewCommentRepository(db)
-	analysisRepo := storage.NewAnalysisRepository(db)
+	overviewRopo := storage.NewOverviewRepository(db)
 
-	comments := collector.CollectorMockComments()
-
-	for _, comment := range comments {
-		_ = commentRepo.Save(c, comment)
-
-		analysis := domain.CommentAnalysis{
-			CommentID: comment.ID,
-			Sentiment: domain.Sentiment(processing.AnalizeSentiment(comment.Message)),
-			Intent:    domain.Intent(processing.DetectIntent(comment.Message)),
-		}
-
-		_ = analysisRepo.Save(c, analysis)
+	result, err := overviewRopo.GetOverview(c)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
 	}
+
+	c.JSON(http.StatusOK, result)
+
 }
